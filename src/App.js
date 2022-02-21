89d10bdafb0f50fbcba87f5709dc223dff071498
Juho Ahonen <juho.ahonen2@gmail.com>
2022-02-21 16:41:32 +0200

    numberfacts v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+const URL = "http://numbersapi.com/random/trivia?json";
 
 function App() {
+  const [type, setType] = useState("trivia")
+  const [fact, setFact] = useState("")
+  const [number, setNumber] = useState(0)
+
+  useEffect(() => {
+    axios.get(URL)
+      .then((response) => {
+        setNumber(response.data.number)
+        setFact(response.data.text)
+      }).catch (error => {
+        alert(error);
+      })
+  }, [])
+  
+  function newFact(e){
+    e.preventDefault();
+      const chosenNumberURL = "http://numbersapi.com/" + number + "/" + type + "?json"
+      axios.get(chosenNumberURL)
+        .then((response) => {
+          setNumber(response.data.number)
+          setFact(response.data.text)
+        }).catch (error => {
+          alert(error);
+    }, [])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={newFact}>
+        <h1>Random number fact</h1>
+        <p>Given number <span id="givenNumber">{number}</span></p>
+        <p id='fact'>{fact}</p>
+        <h2>Choose a new number</h2>
+        <input type="number" onChange={(e) => setNumber(e.target.value)} />
+        <h2>Fact category</h2>
+        <ul>
+          <li>
+            <label for="trivia">Trivia</label>
+            <input type="radio" id="trivia" defaultChecked name="category" value="trivia" onChange={(e) => setType(e.target.value)} />
+            </li>
+          <li>
+            <label for="math">Math</label>
+            <input type="radio" id="math" name="category" value="math" onChange={(e) => setType(e.target.value)} />
+            </li>
+          <li>
+            <label for="year">Year</label>
+            <input type="radio" id="year" name="category" value="year" onChange={(e) => setType(e.target.value)} />
+            </li>
+        </ul>
+        <button>New fact</button>
+    </form>
   );
 }
+
 
 export default App;
